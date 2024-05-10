0850dfd0c00c90801759554c938ea4e7c7f2573a
Vinoth Sivaperumal <vinothsivaperumal@gmail.com>
2024-05-10 16:35:24 -0400

    commit

--- app.py
+++
@@ -1,6 +0,0 @@
-from mod import CustomException
-
-try:
-    raise CustomException('text')
-except CustomException as error:
-    print(error.value)
---
+++ common/custom_exceptions.py
@@ -0,0 +1,2 @@
+class TableNotFoundError(Exception):
+    """Exception raised when a table is not found in the database."""
